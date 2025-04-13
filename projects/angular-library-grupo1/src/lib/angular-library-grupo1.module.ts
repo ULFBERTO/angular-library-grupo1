66fa34d6aa5ab32a187b0e8cf8522ca550ec9765
Jose Harlean Guaneme <jakeinvader2@hotmail.com>
2025-04-12 19:37:15 -0500

    upload created component modal

--- projects/angular-library-grupo1/src/lib/angular-library-grupo1.module.ts
+++ projects/angular-library-grupo1/src/lib/angular-library-grupo1.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { TableComponent } from './table/table.component';
+import { ModalComponent } from './modal/modal.component';
 @NgModule({
-  imports: [CommonModule, ButtonComponent, TableComponent],
-  exports: [ButtonComponent, TableComponent],
+  imports: [CommonModule, ButtonComponent, TableComponent, ModalComponent],
+  exports: [ButtonComponent, TableComponent, ModalComponent],
   providers: []
 })
 export class AngularLibraryGrupo1Module { }
--- projects/angular-library-grupo1/src/public-api.ts
+++ projects/angular-library-grupo1/src/public-api.ts
@@ -7,6 +7,7 @@ export * from './lib/angular-library-grupo1.component';
 export * from './lib/angular-library-grupo1.module';
 export * from './lib/button/button.component';
 export * from './lib/table/table.component';
+export * from './lib/modal/modal.component';
 
 // Exportar rutas de estilos
 export * from './lib/styles/styles';
